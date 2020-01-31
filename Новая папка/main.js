0b967ad556025128ca74a2291d6dad217b61c938
NeitorPerup <60506072+NeitorPerup@users.noreply.github.com>
2020-01-31 21:25:21 +0400

    Create main.js

---
+++ Новая папка/main.js
@@ -0,0 +1,11 @@
+$('document').ready(function(){
+	$('.part').hover (
+		function(){
+			$('.description').html($(this).attr('description-data'));
+			$('.description').fadeIn();
+		},
+		function() {
+			$('.description').fadeOut(50);
+		}
+	)
+});
