b96d991d158c078b803fb8eedfd42c4da7729d2f
NeitorPerup <60506072+NeitorPerup@users.noreply.github.com>
2020-02-04 19:20:36 +0400

    Update main.js

--- Новая папка/main.js
+++ Новая папка/main.js
@@ -1,15 +1,20 @@
 $('document').ready(function(){
-	$('.part').hover (
-		function(){
-			$('.description').html($(this).attr('description-data'));
-			$('.description').fadeIn(50);
-		},
-		function() {
-			$('.description').fadeOut(50);
-		}
-	)
+	// $('.part').hover (
+		// function(){
+			// $('.description').html($(this).attr('description-data'));
+			// $('.description').fadeIn(50);
+		// },
+		// function() {
+			// $('.description').fadeOut(50);
+		// }
+	// )
+	
+	// $('.part').on('click', function() {
+		// alert('Ты жмякнул по кнопке')
+	// })
 	
 	$('.part').on('click', function() {
-		alert('Ты жмякнул по кнопке')
+		$('.description').html($(this).attr('description-data'));
+		$('.description').fadeIn(50);
 	})
 });
