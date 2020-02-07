b631a396fa27b9956f76b815d93f4415ec0d8317
Orturio <60659438+Orturio@users.noreply.github.com>
2020-02-07 20:35:42 +0400

    Update main.js

--- Новая папка/main.js
+++ Новая папка/main.js
@@ -1,15 +1,29 @@
 $('document').ready(function(){
-	$('.part').hover (
-		function(){
-			$('.description').html($(this).attr('description-data'));
-			$('.description').fadeIn(50);
-		},
-		function() {
-			$('.description').fadeOut(50);
-		}
-	)
+	// $('.part').hover (
+		// function(){
+			// $('.description').html($(this).attr('description-data'));
+			// $('.description').fadeIn(50);
+		// },
+		// function() {
+			// $('.description').fadeOut(50);
+		// }
+	// )
+
 	
 	$('.part').on('click', function() {
-		alert('Ты жмякнул по кнопке')
+		$('.description').html($(this).attr('description-data'));
+		$('.description').fadeIn(50);
 	})
+
+	jQuery(function($){
+	$(document).mouseup(function (e){ // событие клика по веб-документу
+		var div = $(".description"); // тут указываем ID элемента
+		if (!div.is(e.target) // если клик был не по нашему блоку
+		    && div.has(e.target).length === 0) { // и не по его дочерним элементам
+			div.hide(); // скрываем его
+		}
+	});
+	
+	
+});
 });
